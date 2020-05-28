1e8925ee7be86b90f39a8bcacd2c713cfe496385
amatke <lexamatovic@gmail.com>
2020-05-28 14:04:26 +0200

    -added Shopping cart feature -added Common class that is @ControllerAdvice -added css.style for classes productAdded and .hide -added to fragment categories fragment for cart info -ajax call for fadeIn fadeOut popUp when product added

--- src/main/java/com/matovic/cmsshoppingcart/Common.java
+++ src/main/java/com/matovic/cmsshoppingcart/Common.java
@@ -2,6 +2,7 @@ package com.matovic.cmsshoppingcart;
 
 import com.matovic.cmsshoppingcart.models.CategoryRepository;
 import com.matovic.cmsshoppingcart.models.PageRepository;
+import com.matovic.cmsshoppingcart.models.entities.Cart;
 import com.matovic.cmsshoppingcart.models.entities.Category;
 import com.matovic.cmsshoppingcart.models.entities.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 
 @ControllerAdvice       // ova klasa presrece sve ostale pozive kontrolera (prvo se ovo izvrsi)
@@ -21,11 +24,30 @@ public class Common {
     private CategoryRepository categoryRepository;
 
     @ModelAttribute
-    public void sharedData(Model model){
+    public void sharedData(HttpSession session, Model model){
 
         List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
         List<Category> categories = categoryRepository.findAll();
 
+        boolean cartActive = false;
+
+        if(session.getAttribute("cartMap") != null){
+            HashMap<Integer, Cart> cartMap = (HashMap<Integer, Cart>) session.getAttribute("cartMap");
+
+            int quantity = 0;
+            double totalPrice = 0;
+
+            for (Cart c : cartMap.values()) {
+                quantity += c.getQuantity();
+                totalPrice += c.getQuantity() * Double.parseDouble(c.getPrice());
+            }
+            model.addAttribute("cquantity", quantity);
+            model.addAttribute("ctotalPrice", totalPrice);
+
+            cartActive = true;
+        }
+
+        model.addAttribute("cartActive", cartActive);
         model.addAttribute("cpages", pages);
         model.addAttribute("ccategories", categories);
     }
